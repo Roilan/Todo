19d210d8e4a1ff2853af850f63388bb263978bd7
Roilan <roilan14@gmail.com>
2014-06-20 00:50:03 -0400

    added modify button for existing task items
    Signed-off-by: Roilan <roilan14@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
 	var taskList = [];
+	var btn = '<button class="btn-lst-mod">Modify</button>';
+
+	// Calls click on parent element 'ul' for button 'modify'
+	$('ul').on('click', '.btn-lst-mod', function() {
+		var x = prompt('Change task');
+		x = '<li>' + x + btn + '</li>';
+
+		//calls btn parent li and replaces with modified prompt
+		$(this).parents('li').replaceWith(x);
+	});
 
 	$('.btn-add').click(function() {
 		var x = prompt('Enter task');
+
 		taskList.push(x);
 
-		var newItem = '<li>' + x + '</li>';
+		var newItem = '<li>' + x + btn + '</li>';
 
 		$('ul').append(newItem);
 
