c5209f294460637a1dba8ef80eccc31c8e9e233c
Roilan <roilan14@gmail.com>
2014-06-20 03:02:09 -0400

    added clear last task button, moved completed task to it's own function
    Signed-off-by: Roilan <roilan14@gmail.com>

--- js/main.js
+++ js/main.js
@@ -3,33 +3,48 @@ $(document).ready(function() {
 	var taskList = [];
 	var btn = '<button class="btn-lst-mod">Modify</button>';
 
-	// Calls click on parent element 'ul' for button 'modify'
+	function clickList() {
+		// allows li to be marked completed(slash) on click
+		$('li').click(function() {
+			$(this).css('text-decoration', 'line-through');
+		});
+	}
+
 	$('ul').on('click', '.btn-lst-mod', function() {
+		// calls prompt for changing task, adds to li with button
 		var x = prompt('Change task');
 		x = '<li>' + x + btn + '</li>';
 
-		//calls btn parent li and replaces with modified prompt
+		// replaces current li with modified
 		$(this).parents('li').replaceWith(x);
+
+		clickList();
 	});
 
 	$('.btn-add').click(function() {
+		// calls prompt for task, adds to li and array
 		var x = prompt('Enter task');
-
 		taskList.push(x);
 
-		var newItem = '<li>' + x + btn + '</li>';
+		// adds new task to end of list
+		var newTask = '<li>' + x + btn + '</li>';
+		$('ul').append(newTask);
 
-		$('ul').append(newItem);
-
-		$('li').css('color', 'black');
-		$('li').click(function() {
-			$(this).css('color', 'red');
-		});
+		clickList();
 	});
 
 	$('.btn-clear').click(function() {
+		//clear array and ul
 		taskList = [];
 
 		$('ul').text('');
+	});
+
+	$('.btn-clear-last').click(function() {
+		// remove last li
+		$('ul li:last-child').remove();
+
+		//remove last li from array
+		taskList.pop();
 	});
 });
